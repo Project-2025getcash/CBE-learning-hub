4cffa470e51f5b85bcb3b6e535459d797346b950
Project-2025getcash <victorotwori88@gmail.com>
2026-06-02 16:17:54 +0300

    Add API utilities

---
+++ backend/src/utils/api.js
@@ -0,0 +1,40 @@
+/**
+ * API Response Utility
+ * Standardized response format for all API endpoints
+ */
+
+export class ApiResponse {
+  constructor(statusCode, data, message = 'Success') {
+    this.statusCode = statusCode;
+    this.data = data;
+    this.message = message;
+    this.success = statusCode < 400;
+  }
+}
+
+/**
+ * API Error Utility
+ * Standardized error response format
+ */
+export class ApiError extends Error {
+  constructor(statusCode, message = 'Something went wrong', errors = []) {
+    super(message);
+    this.statusCode = statusCode;
+    this.errors = errors;
+    this.success = false;
+  }
+}
+
+/**
+ * Async Handler Wrapper
+ * Wraps async route handlers to catch errors
+ */
+export const asyncHandler = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
+};
+
+export default {
+  ApiResponse,
+  ApiError,
+  asyncHandler,
+};
